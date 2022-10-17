82a7f020f6cbf654f421b7b60dee78d20ca51140
DmitriyRim <dimitri.rim@yandex.ru>
2022-10-17 22:26:33 +0300

    ad

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,62 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor(){
+    this._root = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if(this.root() === null) {this._root = new Node(data)};
+    let nextNode = this.root();
+      while(nextNode !== null){
+        if(nextNode.data < data){
+          nextNode = nextNode.right;
+        }else if(nextNode.data > data){
+          nextNode = nextNode.left;
+        }else{
+          break;
+        }
+      nextNode = new Node(data);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let nextNode = this.root();
+    while(nextNode !== null){
+      if(nextNode.data == data) {return false}
+      if(nextNode.data < data){
+        nextNode = nextNode.right;
+      }else if(nextNode.data > data){
+        nextNode = nextNode.left;
+      }else{
+        break;
+      }
+    }
+    return true;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let nextNode = this.root();
+    while(nextNode !== null){
+      if(nextNode.data == data) {return nextNode}
+      if(nextNode.data < data){
+        nextNode = nextNode.right;
+      }else if(nextNode.data > data){
+        nextNode = nextNode.left;
+      }else{
+        break;
+      }
+    }
+    return true;
   }
 
   remove(/* data */) {
